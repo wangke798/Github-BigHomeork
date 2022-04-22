8e1207e60d3ed2ddd4ed9f2ea4fc56bc70c84c4b
songhonglili <3043600864@qq.com>
2022-04-22 13:25:53 +0800

    登录界面修改1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_1);
 
         boolean  a; /*第一次修改*/
         //第二次修改
