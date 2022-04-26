cfc21f2c4a9f6fa00dae3ed3e21b4ac20ffafa93
Lenovo <git@github.com:Wang010824/Wang010824.git>
2022-04-26 18:46:09 +0800

    修改图标以及整体框架

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,22 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button home_button_signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_1);
+        setContentView(R.layout.activity_main);
 
         home_button_signin=findViewById(R.id.home_button_signin);
-        home_button_signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent home_intent=new Intent(MainActivity.this,FunctionActivity.class);
-                startActivity(home_intent);
-            }
+        home_button_signin.setOnClickListener(view -> {
+            Intent home_intent=new Intent(MainActivity.this,FunctionActivity.class);
+            startActivity(home_intent);
         });
     }
 }
