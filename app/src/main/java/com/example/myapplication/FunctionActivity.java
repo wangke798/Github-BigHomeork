c013ac6da82799486352c27fa9a07b3e4e1013af
Lenovo <git@github.com:Wang010824/Wang010824.git>
2022-04-27 13:41:08 +0800

    更正了一些命名问题

--- app/src/main/java/com/example/myapplication/FunctionActivity.java
+++ app/src/main/java/com/example/myapplication/FunctionActivity.java
@@ -10,10 +10,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class FunctionActivity extends AppCompatActivity {
-    private TextView function_imageview1;
-    private TextView function_imageview2;
-    private TextView function_imageview3;
-    private TextView function_imageview4;
+    private TextView function_textview1;
+    private TextView function_textview2;
+    private TextView function_textview3;
+    private TextView function_textview4;
     FragmentTransaction function_fragmenttransaction=getSupportFragmentManager().beginTransaction();
     Fragment function_fragment=null;
 
@@ -22,15 +22,15 @@ public class FunctionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuction);
 
-        function_imageview1=findViewById(R.id.function_imageview1);
-        function_imageview2=findViewById(R.id.function_imageview2);
-        function_imageview3=findViewById(R.id.function_imageview3);
-        function_imageview4=findViewById(R.id.function_imageview4);
+        function_textview1=findViewById(R.id.function_imageview1);
+        function_textview2=findViewById(R.id.function_imageview2);
+        function_textview3=findViewById(R.id.function_imageview3);
+        function_textview4=findViewById(R.id.function_imageview4);
 
-        function_imageview1.setOnClickListener(function_listener);
-        function_imageview2.setOnClickListener(function_listener);
-        function_imageview3.setOnClickListener(function_listener);
-        function_imageview4.setOnClickListener(function_listener);
+        function_textview1.setOnClickListener(function_listener);
+        function_textview2.setOnClickListener(function_listener);
+        function_textview3.setOnClickListener(function_listener);
+        function_textview4.setOnClickListener(function_listener);
         function_fragment=new Fragment1();
         function_fragmenttransaction.add(R.id.function_framelayout,function_fragment);
         function_fragmenttransaction.commit();
@@ -38,10 +38,10 @@ public class FunctionActivity extends AppCompatActivity {
 
     //重置所有文本的选中状态
     public void selected(){
-        function_imageview1.setSelected(false);
-        function_imageview2.setSelected(false);
-        function_imageview3.setSelected(false);
-        function_imageview4.setSelected(false);
+        function_textview1.setSelected(false);
+        function_textview2.setSelected(false);
+        function_textview3.setSelected(false);
+        function_textview4.setSelected(false);
     }
 
 
@@ -49,22 +49,22 @@ public class FunctionActivity extends AppCompatActivity {
         switch (view.getId()){
             case R.id.function_imageview1:
                 selected();
-                function_imageview1.setSelected(true);           //使点击的状态览变化
+                function_textview1.setSelected(true);           //使点击的状态览变化
                 function_fragment=new Fragment1();
                 break;
             case R.id.function_imageview2:
                 selected();
-                function_imageview2.setSelected(true);
+                function_textview2.setSelected(true);
                 function_fragment=new Fragment2();
                 break;
             case R.id.function_imageview3:
                 selected();
-                function_imageview3.setSelected(true);
+                function_textview3.setSelected(true);
                 function_fragment=new Fragment3();
                 break;
             case R.id.function_imageview4:
                 selected();
-                function_imageview4.setSelected(true);
+                function_textview4.setSelected(true);
                 function_fragment=new Fragment4();
                 break;
         }
