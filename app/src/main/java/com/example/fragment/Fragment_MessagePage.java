a7a521c369965e9ab9fa4b14de9ae2a3c18045be
xiaoxiongxubaba <2993960464@qq.com>
2022-05-03 20:16:48 +0800

    Fragment3

--- app/src/main/java/com/example/fragment/Fragment_MessagePage.java
+++ app/src/main/java/com/example/fragment/Fragment_MessagePage.java
@@ -5,11 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.annotation.ContentView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.activity.MessageDatabase;
@@ -29,10 +31,15 @@ public class Fragment_MessagePage extends Fragment {
     private int header1 = R.drawable.header;
     private int header2 = R.drawable.header2;
     private MessageDatabase message;
-
+    private DrawerLayout drawerLayout;
+    private LinearLayout view_left;
+    private LinearLayout view_right;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment3,container,false);
 
+
+        //将message添加到list中
         fragment3_listview = view.findViewById(R.id.fragment3_listview);
         for(int i = 0; i < 30; i ++){
             if(i % 2 == 0){
@@ -43,6 +50,36 @@ public class Fragment_MessagePage extends Fragment {
             list.add(message);
         }
 
+
+        //添加滑动事件
+//        drawerLayout = view.findViewById(R.id.DrawLayout);
+//        view_left = view.findViewById(R.id.view_left);
+//        view_right = view.findViewById(R.id.view_right);
+//        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+//            @Override
+//            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+//
+//
+//                view_left.setTranslationX(-(drawerView.getMeasuredWidth()*(slideOffset)));
+//            }
+//
+//            @Override
+//            public void onDrawerOpened(@NonNull View drawerView) {
+//
+//            }
+//
+//            @Override
+//            public void onDrawerClosed(@NonNull View drawerView) {
+//
+//            }
+//
+//            @Override
+//            public void onDrawerStateChanged(int newState) {
+//
+//            }
+//        });
+
+        //为页面添加baseAdapter
         Fragment3BaseAdapter baseAdapter = new Fragment3BaseAdapter(fragment3_listview.getContext(), list);
         fragment3_listview.setAdapter(baseAdapter);
         fragment3_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
