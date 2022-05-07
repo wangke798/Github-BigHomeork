b42b5f3675a0aecd35ad025fa4e41aa12123095e
xiaoxiongxubaba <2993960464@qq.com>
2022-05-07 18:22:18 +0800

    fragment3添加长按删除功能

--- app/src/main/java/com/example/fragment/Fragment_MessagePage.java
+++ app/src/main/java/com/example/fragment/Fragment_MessagePage.java
@@ -1,5 +1,7 @@
 package com.example.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.ContentView;
 import androidx.annotation.NonNull;
@@ -30,6 +33,7 @@ public class Fragment_MessagePage extends Fragment {
     private int header1 = R.drawable.header;
     private int header2 = R.drawable.header2;
     private MessageDatabase message;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment3,container,false);
@@ -52,10 +56,34 @@ public class Fragment_MessagePage extends Fragment {
         fragment3_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+        });
+
+        fragment3_listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(fragment3_listview.getContext());
+                builder.setTitle("提示")
+                        .setMessage("确定删除吗？")
+                        .setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                list.remove(position);
+                                baseAdapter.notifyDataSetChanged();
+                                Toast.makeText(getContext(),"已删除",Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("取消", null)
+                        .create().show();
+                return true;
             }
         });
 
 
         return view;
     }
+
+
 }
