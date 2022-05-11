5a90f13065d0f44e94575fe71c95f6ca9dad8b86
Lenovo <git@github.com:Wang010824/Wang010824.git>
2022-05-11 15:19:15 +0800

    忘记密码和注册页面的编写

--- app/src/main/java/com/example/activity/MainActivity.java
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -14,12 +14,14 @@ import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button home_button_signin;
+    public Button home_button_signin;
+    public Button register_button;
+    public Button findback_password_button;
 
-    com.example.Fragment.Fragment_FirstPage Fragment_FirstPage = new Fragment_FirstPage();
-    com.example.Fragment.Fragment_PublishPage Fragment_PublishPage = new Fragment_PublishPage();
-    com.example.Fragment.Fragment_MessagePage Fragment_MessagePage = new Fragment_MessagePage();
-    com.example.Fragment.Fragment_HomePage Fragment_HomePage = new Fragment_HomePage();
+    Fragment_FirstPage Fragment_FirstPage = new Fragment_FirstPage();
+    Fragment_PublishPage Fragment_PublishPage = new Fragment_PublishPage();
+    Fragment_MessagePage Fragment_MessagePage = new Fragment_MessagePage();
+    Fragment_HomePage Fragment_HomePage = new Fragment_HomePage();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +29,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         home_button_signin=findViewById(R.id.home_button_signin);
+        register_button = findViewById(R.id.register_button);
+        findback_password_button = findViewById(R.id.findback_password_button);
 
         home_button_signin.setOnClickListener(view -> {
             Intent home_intent=new Intent(MainActivity.this,FunctionActivity.class);
             startActivity(home_intent);
+        });
+
+        register_button.setOnClickListener(view -> {
+            Intent register = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(register);
+        });
+
+        findback_password_button.setOnClickListener(view -> {
+            Intent findbackPassword = new Intent(MainActivity.this, FindPasswordActivity.class);
+            startActivity(findbackPassword);
         });
     }
 
