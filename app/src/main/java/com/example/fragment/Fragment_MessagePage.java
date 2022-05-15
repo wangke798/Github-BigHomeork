1554c32707b3373879a025fa693815e705fd3c47
Lenovo <git@github.com:Wang010824/Wang010824.git>
2022-05-15 20:10:39 +0800

    第四个页面的编写

--- app/src/main/java/com/example/fragment/Fragment_MessagePage.java
+++ app/src/main/java/com/example/fragment/Fragment_MessagePage.java
@@ -7,14 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.annotation.ContentView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.Activity.MessageDatabase;
